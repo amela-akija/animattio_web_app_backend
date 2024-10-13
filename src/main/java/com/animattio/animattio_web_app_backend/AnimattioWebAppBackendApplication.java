9e5aec31cd9d3603d79c85294aa95fcb570cf90a
Amela <aakija2002@gmail.com>
2024-10-13 19:16:07 +0200

    initial commit

---
+++ src/main/java/com/animattio/animattio_web_app_backend/AnimattioWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.animattio.animattio_web_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimattioWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimattioWebAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animattio/animattio_web_app_backend/AnimattioWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animattio.animattio_web_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimattioWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
