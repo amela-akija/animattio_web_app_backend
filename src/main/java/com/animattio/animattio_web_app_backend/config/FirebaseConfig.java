e189771676ac61243c002fd493a24c1fcad8e8b9
Amela <aakija2002@gmail.com>
2024-12-30 18:29:38 +0100

    test

--- src/main/java/com/animattio/animattio_web_app_backend/config/FirebaseConfig.java
+++ src/main/java/com/animattio/animattio_web_app_backend/config/FirebaseConfig.java
@@ -9,21 +9,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${custom.firebase_keys}")
-    private String firebaseKeysPath;
+//    @Value("${custom.firebase_keys}")
+//    private String firebaseKeysPath;
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource(firebaseKeysPath).getInputStream();
+            String serviceAccountKey = System.getenv("FIREBASE_SERVICE_ACCOUNT");
+            if (serviceAccountKey == null || serviceAccountKey.isEmpty()) {
+                throw new IllegalStateException("Environment variable 'FIREBASE_SERVICE_ACCOUNT' is not set.");
+            }
+
+            ByteArrayInputStream serviceAccountStream =
+                    new ByteArrayInputStream(serviceAccountKey.getBytes(StandardCharsets.UTF_8));
+
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                     .build();
             FirebaseApp.initializeApp(options);
         }
